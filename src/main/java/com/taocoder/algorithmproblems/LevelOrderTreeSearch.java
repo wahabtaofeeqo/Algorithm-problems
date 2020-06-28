2e64bb5c736ca42c590d2ddb58f44d75c75d998a
Taofeek <taofeekolamilekan218@gmail.com>
2020-06-28 03:51:51 +0100

    Level order search added

---
+++ src/main/java/com/taocoder/algorithmproblems/LevelOrderTreeSearch.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.taocoder.algorithmproblems;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ *
+ * @author user
+ */
+public class LevelOrderTreeSearch {
+    
+    private Node root;
+    private Queue<Node> nodes;
+    
+    public LevelOrderTreeSearch(Node root) {
+        this.root = root;
+        nodes = new LinkedList<>();
+        if(root != null) nodes.add(root);
+    }
+    
+    public void printLevel() {
+        
+        while(!nodes.isEmpty()) {
+            
+            Node current = nodes.poll();
+            System.out.print(current.getData() + " ");
+            
+            if(current.left != null) {
+                addNode(current.left);
+            }
+            
+            if(current.right != null) {
+                addNode(current.right);
+            }
+        }
+    }
+    
+    public void addNode(Node node) {
+        nodes.add(node);
+    }
+}
+
+class Node {
+    
+    private int data;
+    Node left, right;
+    
+    public Node(int item) {
+        this.data = item;
+        this.left = null;
+        this.right = null;
+    }
+    
+    public int getData() {
+        return data;
+    }
+}
--- src/main/java/com/taocoder/algorithmproblems/Main.java
+++ src/main/java/com/taocoder/algorithmproblems/Main.java
@@ -52,6 +52,18 @@ public class Main {
         
         
         System.out.println("\n\n======Bubble sort=======");
-         System.out.println(Arrays.toString(BubbleSort.sort(new int[]{12, 11, 2, 3, 1, 30, 5, 20, 50})));
+        System.out.println(Arrays.toString(BubbleSort.sort(new int[]{12, 11, 2, 3, 1, 30, 5, 20, 50})));
+        
+        System.out.println("\n\n=====Level Order Tree Search=====");
+        
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        
+        LevelOrderTreeSearch lots = new LevelOrderTreeSearch(root);
+        lots.printLevel();
     }
 }
